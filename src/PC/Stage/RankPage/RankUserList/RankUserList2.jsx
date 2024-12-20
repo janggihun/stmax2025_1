61bb5900a71f7e3df558188e956f90eb4a254ce3
janggihun <jgh7111@gmail.com>
2024-12-21 03:35:48 +0900

    올콤보 추가

--- src/PC/Stage/RankPage/RankUserList/RankUserList2.jsx
+++ src/PC/Stage/RankPage/RankUserList/RankUserList2.jsx
@@ -93,7 +93,7 @@ export const RankUserList2 = (props) => {
                       {el.titleName && el.titleName}
                     </div>
                     <div className="RankUser_UserId">{el.userId}</div>
-                    {el.miss === 0 ? (
+                    {el.stmax0 === 0 ? (
                       <div className="RankUser_Blank2 ShowRank_AllCombo"> All Combo</div>
                     ) : (
                       <div className="RankUser_Blank2"></div>
